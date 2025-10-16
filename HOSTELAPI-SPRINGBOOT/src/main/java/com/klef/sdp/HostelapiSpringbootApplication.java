6efba64ed37893caa7d425e53e2d3b1370ab47ef
Nikhil <2300030869@kluniversity.in>
2025-10-16 22:55:28 +0530

    Updated SpringBootApplication file

--- HOSTELAPI-SPRINGBOOT/src/main/java/com/klef/sdp/HostelapiSpringbootApplication.java
+++ HOSTELAPI-SPRINGBOOT/src/main/java/com/klef/sdp/HostelapiSpringbootApplication.java
@@ -2,8 +2,8 @@ package com.klef.sdp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
 
 @SpringBootApplication
 public class HostelapiSpringbootApplication extends SpringBootServletInitializer{
